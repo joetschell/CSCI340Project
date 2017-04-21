c73ee69144bfa29d63ca4af61b4a47dcc72ca552
joetschell <joetschell@hotmail.com>
2017-04-21 15:55:10 -0400

    add idea to project.c file

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ project: project.o
 			gcc project.o -o project
 
 project.o: project.c
-			gcc -c -02 Wall -g project.c
+			gcc -c -Wall -g project.c
 
 clean:
 			/bin/rm -f project *.o *.gz
--- project.c
+++ project.c
@@ -5,3 +5,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include "project.h"
+
+/*
+IDEA:
+1. create 128 capacity array with each index containing a data type of 1 byte
+2. access each index in order and time it
+3. print out the index and the time it took to access
+4. try to see if there was a big change in access time on indices that are a power of 2
+*/
