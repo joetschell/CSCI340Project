20e2ef8702c3e597c6f60afce871a617764f5d8f
joetschell <joetschell@hotmail.com>
2017-04-23 14:53:49 -0400

    access time trial

--- project.c
+++ project.c
@@ -15,19 +15,46 @@ IDEA:
 3. print out the index and the time it took to access, and the address
 4. try to see if there was a big change in access time on indices that are a power of 2
 */
+int calc_line_size(){
+int nums[128];
+int i = 0;
+clock_t start = clock(), diff;
+//initialize the nums array
+for(i = 0; i<128; i++){
+    nums[i] = i;
+}
+
+//get the access time for each num
+for(i = 0; i<128; i++){
+    start = clock();
+    printf("access time for num %d:  ", i);
+    diff = clock() - start;
+    printf("%lu\n", diff * 1000000000 / CLOCKS_PER_SEC);
+
+}
+return 0;
+}//end of calc_line_size
+
 
 
 int main(int argc, char* argv[]){
     //testing how to use the clock function:
 int i = 0;
+int j = 0;
 clock_t start = clock(), diff;
 
-for(i = 0; i<0; i++){
-    ;
-}
+// for(i = 0; i<500; i++){
+//     for(j = 0; j<10000000; j++){
+//         ;
+//     }
+//     ;
+// }
 diff = clock() - start;
 
-printf("time elapsed in nano sec: %lu ns\n", diff * 1000000000 / CLOCKS_PER_SEC);
+printf("time elapsed in nano sec: %lu ns\n", diff  * 1000000000 / CLOCKS_PER_SEC);
+
+
+calc_line_size();
 
     return 0;
 }
